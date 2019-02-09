75307c64175a3f0c6157a1f07402bb4f6e74f030
Zhanarys <zhubanaliev01@gmail.com>
2019-02-09 14:01:58 +0300

    first commit

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("C:\\input.txt");
+            String s = sr.ReadToEnd();
+            Console.WriteLine(s);
+            //  Console.ReadKey();
+
+
+            string[] arr = s.Split();
+
+
+            string ss = "";
+            for (int j = 0; j < arr.Length; j++)
+            {
+                int n = int.Parse(arr[j]);
+                bool isPrime = true;
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (n != 1 && isPrime)
+                {
+
+                    ss += Convert.ToString(n) + " ";
+
+                }
+            }
+
+            StreamWriter sw = new StreamWriter("C:\\output.txt");
+            sw.WriteLine(ss);
+            sw.Close();
+        }
+        }
+}
---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void PrintSpaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+                Console.Write("  ");
+        }
+        static void FD(DirectoryInfo directory, int level)
+        {
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo d in directories)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(d.Name);
+                FD(d, level + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo("C:/Users/Жанарыс/Desktop/Week2");
+            FD(d, 0);
+            Console.ReadKey();
+
+        }
+    }
+}
